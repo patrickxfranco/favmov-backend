ff09af8bef9efb2e0ea74a15074380b05ed25e00
Patrick Franco <patrickxfranco@gmail.com>
2024-11-02 13:35:01 -0300

    Adiciona o CORS como dependência

--- source/server.js
+++ source/server.js
@@ -4,10 +4,12 @@ import routes from "./routes/index.js";
 import { exec } from "child_process";
 import "express-async-errors";
 import express from "express";
+import cors from "cors";
 
 exec("npx knex migrate:latest");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
